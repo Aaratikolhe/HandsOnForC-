9969aee633a0fb2dcd8be03e330d39d64d5b15d6
Aaratikolhe <kolhe.aarati@gmail.com>
2022-07-01 17:48:19 +0530

    ReadJsonFile project added to read the json file and modification in twoPairWithEqualSum project

---
+++ ReadJsonFile1/Product.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReadJsonFile1
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public ProductProperties ProductDescription { get; set; }
+        public int ProductPrice { get; set; }
+        public int ProductQuantity { get; set; }
+
+        public override string? ToString()
+        {
+            return $"{ProductId}  {ProductId} {ProductDescription}";
+        }
+    }
+}
---
+++ ReadJsonFile1/ProductProperties.cs
@@ -0,0 +1,10 @@
+﻿namespace ReadJsonFile1
+{
+    public class ProductProperties
+    {
+        public string Color { get; set; }
+        public int Height { get; set; }
+        public int Width { get; set; }
+        public string Category { get; set; }    
+    }
+}
