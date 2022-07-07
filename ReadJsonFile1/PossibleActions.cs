a115dc04d92976fa5da0224e3cc3e1d8d318ba2a
Aaratikolhe <kolhe.aarati@gmail.com>
2022-07-07 16:20:10 +0530

    PairsWithEqualSum project modified to get output using dictionary

--- ReadJsonFile1/PossibleActions.cs
+++ ReadJsonFile1/PossibleActions.cs
@@ -2,8 +2,8 @@
 {
     public class PossibleActions
     {
-        public string? Name { get; set; }
-        public List<Properties>? Properties { get; set; }
+        public string Name { get; set; }
+        public List<Properties> Properties { get; set; }
         public PossibleActions(string name, List<Properties> properties)
         {
             Name = name;
---
+++ StringAndStringBuilder/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Text;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Enter any string");
+        string s1 = Console.ReadLine();
+        AppendString(s1);
+        Console.WriteLine("String using string concat  function: " + s1);
+
+        StringBuilder s2 = new StringBuilder("Welcome");
+        AppendStringUsingStringBuilder(s2);
+        Console.WriteLine("String using StringBuilder: " + s2);
+    }
+    public static void AppendStringUsingStringBuilder(StringBuilder str)
+    {
+        str.Append("Aarati");
+    }
+    public static void AppendString(string str)
+    {
+        string str2 = " Aarati";
+        str=String.Concat(str,str2);
+        //return s2;
+    }
+    
+    
+    
+    
+}
