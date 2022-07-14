a34acfeb38a921c8e875f8f07f1a53b4e1ed2408
Aaratikolhe <kolhe.aarati@gmail.com>
2022-07-14 12:21:11 +0530

    UsingNulls modified

--- UsingNulls/Program.cs
+++ UsingNulls/Program.cs
@@ -8,17 +8,28 @@
             //#nullable disable
             string? message = null;
             Console.WriteLine(message);
-            //Student student = new Student()
-            //{
-            //    FirstName = null,
-            //    LastName = "Kolhe"
-            //};
+            Student student = new Student()
+            {
+                FirstName = "Arti",
+                LastName = null
+            };
             //Student student = new Student(null,"Kolhe");
-            Student student = new Student("Aarati", "Kolhe");
+            //Student student = new Student("Aarati", null);
             Console.WriteLine(student.FirstName ?? "No value for firstName");
             Console.WriteLine(student.FirstName!.Length);
             Console.WriteLine(student.LastName);
+            ExitProgramIfNull(student.LastName, "Last name");
+
             Console.WriteLine(student.ToUpperName());
+        }
+
+        public static void ExitProgramIfNull(string? lastName, string description)
+        {
+            if(lastName is null)
+            {
+                Console.WriteLine(description+" is null.Exiting the program...");
+                Environment.Exit(0);
+            }
         }
     }
 }
--- UsingNulls/Student.cs
+++ UsingNulls/Student.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,15 +12,27 @@ namespace UsingNulls
         public int Id { get; set; }
         //public string? FirstName { get; set; }
         public string FirstName { get; set; }
-        public string LastName { get; set; }
+        private string lastName;
+
+        //[DisallowNull]
+        //public string? LastName 
+        //{ 
+        //    get=>lastName;
+        //    set=>lastName=value??throw new ArgumentNullException(nameof(value)); }
+
+        public string LastName
+        {
+            get => lastName;
+            set => lastName = value;
+        }
         public int Age { get; set; }
         public DateTime BirthDate { get; set; }
         public string? ToUpperName()=> FirstName?.ToUpperInvariant();
-        public Student(string firstName, string lastName)
-        {
-            FirstName = firstName;
-            LastName = lastName;
+        //public Student(string firstName, string lastName)
+        //{
+        //    FirstName = firstName;
+        //    LastName = lastName;
 
-        }
+        //}
     }
 }
